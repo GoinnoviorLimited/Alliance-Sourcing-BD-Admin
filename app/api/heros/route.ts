516226a389e575603142e4bdebdac537568fda05
mizanor21 <mizanorrahman398@gmail.com>
2026-03-09 15:17:27 +0600

    refactor hero route

--- app/api/heros/route.ts
+++ app/api/heros/route.ts
@@ -29,29 +29,3 @@ export async function POST(req: Request) {
     );
   }
 }
-
-export async function DELETE(req: Request) {
-  try {
-    const url = new URL(req.url);
-    const id = url.searchParams.get("id");
-    await connectToDB();
-    const deleted = await HeroSection.findByIdAndDelete(id);
-    if (!deleted) {
-      return NextResponse.json(
-        { message: " data not found" },
-        { status: 404 }
-      );
-    }
-
-    return NextResponse.json(
-      { message: " data deleted" },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Failed to delete  data" },
-      { status: 500 }
-    );
-  }
-}
