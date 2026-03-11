dba560a4f12394eefe56c207f9c07e1c52a6a8dd
mizanor21 <mizanorrahman398@gmail.com>
2026-03-11 13:42:30 +0600

    apar manager part polishing

--- components/Home/Apart/ApartManager.jsx
+++ components/Home/Apart/ApartManager.jsx
@@ -90,7 +90,7 @@ export default function ApartManager() {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="">
       {/* Header */}
       <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8">
         <div>
